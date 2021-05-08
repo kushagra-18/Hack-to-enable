e95c295c73bff284462fedb0e99d4271401df0f7
Kushagra Sharma <giantkillerkushagra@gmail.com>
2021-05-09 02:32:19 +0530

    final

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ from keras.preprocessing import image
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
+from flask_gtts import gtts
 from gevent.pywsgi import WSGIServer
 from flask import Response
 from yolo_detection_images import runModel
@@ -32,11 +33,37 @@ from walk import VideoCamera
 
 app = Flask(__name__)
 
+gtts(app)
 
-model = Sequential()
+
+
+
+
+#model = Sequential()
 
 # Model saved with Keras model.save()
 MODEL_PATH = 'mdl_wts_xc.hdf5'
+
+MODEL_PATH_CURRENCY = 'mdl_wts_xc.hdf5'
+
+MODEL_PATH_W = 'mdl_wts_xc.hdf5'
+
+MODEL_PATH_B = 'BrailleNet.h5'
+
+class_labels = {0:'No DR', 1:'Mild',2:'Moderate',3:'Severe',4:'Proliferative DR'}
+
+class_labels_new = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+
+
+class_labels_curr = {0: '10', 1: '100', 2: '20', 3: '200', 4: '2000', 5: '50', 6: '500', 7: 'Background'}
+
+
+class_labels_w = {0: 'Cloudy', 1: 'Sunny', 2: 'Rainy', 3: 'Snowy', 4: 'Foggy'}
+
+model = load_model(MODEL_PATH)
+model_c = load_model(MODEL_PATH_CURRENCY)
+model_w = load_model(MODEL_PATH_W)
+model_b = load_model(MODEL_PATH_B)
 
 
 def model_predict(img_path, model):
@@ -49,6 +76,47 @@ def model_predict(img_path, model):
 
 
     preds = model.predict(x)
+    return preds
+
+
+def model_predict_c(img_path, model):
+    img = image.load_img(img_path, target_size=(150, 150))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+
+    preds = model_c.predict(x)
+    return preds
+
+
+
+def model_predict_w(img_path, model):
+    img = image.load_img(img_path, target_size=(100, 100))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+
+    preds = model_w.predict(x)
+    return preds
+
+
+
+def model_predict_b(img_path, model):
+    img = image.load_img(img_path, target_size=(28, 28))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+
+    preds = model_b.predict(x)
     return preds
 
 ############################################## THE REAL DEAL ###############################################
@@ -72,7 +140,7 @@ def mask_image():
 	img_base64 = base64.b64encode(rawBytes.read())
 	return jsonify({'status':str(img_base64)})
 
-##################################################### THE REAL DEAL HAPPENS ABOVE ######################################
+
 
 @app.route('/test' , methods=['GET','POST'])
 def test():
@@ -82,6 +150,13 @@ def test():
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+
+#--------
+
+
+#---------
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
@@ -96,27 +171,123 @@ def upload():
         f.save(file_path)
 
         # Make prediction
-        preds = model_predict(file_path, model)
+        
+        preds = model_predict(file_path, model) 
+        
+        print(preds)
 
         pred_prob = np.max(preds)
-        pred_class = preds.argmax(axis=-1)            
-        print(preds)
+        
+        pred_class = preds.argmax(axis=-1) 
+        
         result = pred_class[0]
 
-        
+        res = (class_labels[result])
 
-        
-        
-
-        
-        return 'hi'
+ 
+        return res
     return None
 
 
+@app.route('/weather', methods=['GET', 'POST'])
+def upload1():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        
+        preds = model_predict_w(file_path, model) 
+        
+        print(preds)
+
+        pred_prob = np.max(preds)
+        
+        pred_class = preds.argmax(axis=-1) 
+        
+        result = pred_class[0]
+
+        res = (class_labels_w[result])
+
+ 
+        return render_template('weather.html',pred_text_w = res) 
+    return None
+
+#----
+
+@app.route('/braile', methods=['GET', 'POST'])
+def upload2():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        
+        preds = model_predict_b(file_path, model) 
+        
+        print(preds)
+
+        pred_prob = np.max(preds)
+        
+        pred_class = preds.argmax(axis=-1) 
+        
+        result = pred_class[0]
+
+        res = (class_labels_new[result])
+
+ 
+        return render_template('braile.html',pred_text_b = res) 
+    return None
+
+
+#---
+
+@app.route('/currency', methods=['GET', 'POST'])
+def upload3():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        
+        preds = model_predict_c(file_path, model) 
+        
+        print(preds)
+
+        pred_prob = np.max(preds)
+        
+        pred_class = preds.argmax(axis=-1) 
+        
+        result = pred_class[0]
+
+        res = (class_labels_c[result])
+
+ 
+        return render_template('currency.html',pred_text_c = res) 
+    return None
 
 #---open cv-----
 
 def gen(walk):
+    
     while True:
         #get camera frame
         frame = walk.get_frame()
@@ -132,21 +303,36 @@ def video_feed():
 
 @app.route('/index')
 def index():
-	return render_template('index.html')
+	return render_template('yolo.html')
 
 
 @app.route('/dis')
 def dis():
 	return render_template('index.html')
 
-@app.route('/song')
-def song():
-	return render_template('songs.html')
+
+@app.route('/brailePG')
+def brailePG():
+	return render_template('braile.html')
+
+
+@app.route('/weatherPG')
+def weatherPG():
+	return render_template('weather.html')
+
+
+@app.route('/currencyPG')
+def currencyPG():
+	return render_template('currency.html')
 	
     
 @app.route('/audio')
 def audio():
 	return render_template('audio.html')
+
+@app.route('/song')
+def song():
+	return render_template('songs.html')
 
 @app.route('/walk')
 def walk():
---
+++ walkApp.py
@@ -0,0 +1,79 @@
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+import pyttsx3
+engine = pyttsx3.init() 
+
+volume = engine.getProperty('volume')   
+print (volume)                          
+engine.setProperty('volume',1.0)  
+
+
+classifier = load_model('blind.hdf5')
+
+class_labels = ['Forward','Left','Right']
+
+
+
+cap=cv2.VideoCapture(0)
+
+#address = "https://192.168.1.8:8000/video"
+
+#cap.open(address)
+
+
+ds_factor=0.6
+
+
+while True:
+
+    ret,frame=cap.read()
+
+    labels=[]
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+
+    roi_gray = gray
+
+    roi_gray=cv2.resize(roi_gray,(150,150),interpolation=cv2.INTER_AREA)
+
+    if np.sum([roi_gray])!=0:
+
+        roi=roi_gray.astype('float')/255.0
+
+        roi=img_to_array(roi)
+
+        roi=np.expand_dims(roi,axis=0)
+
+        preds=classifier.predict(roi)[0]
+
+
+
+        #res = pred.argmax()
+
+        #if res > .55:
+
+        label=class_labels[preds.argmax()]
+
+        print(label)
+
+        engine.say('Please Move' + str(label))
+        engine.runAndWait()
+        engine.stop()
+
+
+        label_position=(100,100)
+
+        cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+    else:
+        cv2.putText(frame,'No Face Found',(20,20),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+    
+        cv2.imshow('Blind Walk Assistant',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+cap.release()
+cv2.destroyAllWindows()
